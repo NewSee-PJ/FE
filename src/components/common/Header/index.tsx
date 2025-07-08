46168ed86cc7672ecfe6d82cdc3ce8ee9648874b
jungsun <dlwjdtjs0111@gmail.com>
2025-07-08 18:10:00 +0900

    🔀 merge: branch merge

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,23 +1,35 @@
 import * as S from "./index.styles";
-import { HeaderLogo } from "@/assets/icons/common";
-import { useUserStore } from "@/stores/user";
+import { HeaderLogo } from "@assets/icons/common";
+import { useUserStore } from "@stores/user";
 import profileDefaultImage from "@assets/images/profileImage.png";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const token = useUserStore((state) => state.accessToken);
-  const name = useUserStore((state) => state.name);
-  const profileImage = useUserStore((state) => state.profileImage);
+  const { accessToken: token, name, profileImage } = useUserStore();
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
 
   return (
     <S.Container>
-      <HeaderLogo width={109} height={33} />
-      {true ? (
+      <HeaderLogo width={109} height={33} onClick={handleLogoClick} />
+      {token ? (
         <S.UserInfo>
-          <S.UserName>name</S.UserName>
-          <img src={profileDefaultImage} width={32} height={32} />
+          <S.UserName>{name}</S.UserName>
+          <img
+            src={profileImage ? profileImage : profileDefaultImage}
+            width={32}
+            height={32}
+          />
         </S.UserInfo>
       ) : (
-        <S.ButtonContainer>로그인</S.ButtonContainer>
+        <S.ButtonContainer onClick={handleLoginClick}>로그인</S.ButtonContainer>
       )}
     </S.Container>
   );
